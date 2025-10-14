b95cc7a594b838862421824d990b8aea9bb8fbe9
deoz-art <storedeoz@gmail.com>
2025-10-15 06:47:28 +0700

    Fix corrupted postcss.config.js causing deployment failure

--- frontend/postcss.config.js
+++ frontend/postcss.config.js
@@ -1,7 +1,6 @@
-<tr>
-  <td>Username</td>
-  <td>Role</td>
-  <td>Date Joined</td>
-  <td>Scores (View Scores button)</td>
-  <td>Actions</td>
-</tr>
+module.exports = {
+  plugins: {
+    tailwindcss: {},
+    autoprefixer: {},
+  },
+};
